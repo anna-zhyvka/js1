6027ada8317258e05a7b1ed5ed90cd74637db8d0
anna-zhyvka <annazhyvka516@gmail.com>
2022-11-01 13:45:25 +0200

    Initial commit

---
+++ js1/js1.js
@@ -0,0 +1,43 @@
+"use strict";
+//1 завдання
+let first = "Hello";
+console.log(first);
+let second = "owu";
+console.log(second);
+let third = "com";
+console.log(third);
+let fourth = "ua";
+console.log(fourth);
+let fifth = 1;
+console.log(fifth);
+let sixth = 10;
+console.log(sixth);
+let seventh = -999;
+console.log(seventh);
+let eights = 123;
+console.log(eights);
+let ninth = 3.14;
+console.log(ninth);
+let tenth = 2.7;
+console.log(tenth);
+let eleventh = 16;
+console.log(eleventh);
+let twelfth = true;
+console.log(twelfth);
+let thirteenth = false;
+console.log(thirteenth);
+
+//2 завдання
+let firstname = "Анна";
+let middlename = "Степанівна";
+let lastname = 'Живка';
+console.log(`${firstname} ${middlename} ${lastname}`);
+
+//3 завдання
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
